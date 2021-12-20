558cc574dc1de9c932497aa9af3b1b949f266422
Harutyun Manukyan <mnmmkhnkoyan@gmail.com>
2021-12-20 14:22:56 +0400

    prometheus version selector

--- scripts/1-install.sh
+++ scripts/1-install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-PROMETHEUS_VERSION="2.2.1"
+if [[ -z $1 ]]
+then
+  PROMETHEUS_VERSION="2.2.1"
+else
+  PROMETHEUS_VERSION="$1"
+fi
+
 wget https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz
 tar -xzvf prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz
 cd prometheus-${PROMETHEUS_VERSION}.linux-amd64/
